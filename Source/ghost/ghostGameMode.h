c38ac6cc1a8ef94af4a30408e84153a155fbd93d
Andres Schmois <andresdroid@gmail.com>
2016-10-22 18:00:35 -0400

    Initial Commit

---
+++ Source/ghost/ghostGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ghostGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GHOST_API AghostGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
